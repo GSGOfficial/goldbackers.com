c05b88cf0c3d5778760c5eaecca097e478f3c77c
Lucky Rico <76436263+GSGOfficial@users.noreply.github.com>
2023-03-09 07:45:51 -0500

    php

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Contact Form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <thegoldstandardgrp@gmail.com>';
+$sendTo = 'New Contact Form <thegoldstandardgrp@gmail.com>';
 
 // subject of the email
 $subject = 'New message from Goldbackers.com';
